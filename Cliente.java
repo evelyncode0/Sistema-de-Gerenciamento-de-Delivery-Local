bf5ddfd0ade395962809a5b2e5a9078aad35c147
Evelyn <160793847+evelyncode0@users.noreply.github.com>
2026-03-08 21:29:58 -0300

    Parte 4 do trabalho - Herança e Polimorfismo

--- Cliente.java
+++ Cliente.java
@@ -1,52 +1,17 @@
-public class Cliente {
-    //atributos
-    private String nome;
-    private String telefone;
+public class Cliente extends Usuario { //criando subclass da classe pai (usuario) - herança
 
-    //construtor com validações
-    public Cliente(String nome, String telefone){
-        if (nome == null || nome.trim().isEmpty()){ //verifica se o nome é nulo ou com espaçõs e vazio
-            System.out.println("Nome do cliente inválido.");
-            this.nome = "Cliente sem nome."; //sistema define nome invalido 
-        } else {
-            this.nome = nome;
-        }
-
-        if (telefone == null || telefone.trim().isEmpty()){
-            System.out.println("Telefone do cliente inválido.");
-            this.telefone = "Cliente sem telefone.";
-        }else{
-            this.telefone = telefone;
-        }
-       
-       
+    //construtor com atributos da classe pai
+    public Cliente(String nome, String email, String telefone){
+        super(nome, email, telefone); //inicializa os atributos da classe pai
     }
-
-    //getters
-    public String getNome(){
-        return nome;
-    }
-
-    public String getTelefone(){
-        return telefone;
-    }
-
-    //setter opcional
-
-    public void setTelefone(String telefone){ //reforçando validação de telefone para não quebrar a regra de negocio e criar um telefone invalido após alteração no setter.
-        if (telefone == null || telefone.trim().isEmpty()){
-            System.out.println("Telefone inválido");
-        } else{
-            this.telefone = telefone;
-        }
-       
-    }
-
 
     //metodos para dados do cliente
-    void exibirDadosCliente(){
-        System.out.println("Cliente: " + nome);
-        System.out.println("Telefone: " + telefone);
+    @Override //evita bugs para sobrescrever os metodos
+    public void exibirDados(){
+        System.out.println("---DADOS DO CLIENTE---");
+        System.out.println("Cliente: " + getNome());
+        System.out.println("Email: " + getEmail());
+        System.out.println("Telefone: " + getTelefone());
 
     }
 }
